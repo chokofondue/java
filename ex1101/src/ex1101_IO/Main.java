7ca76ada3e668066128f5ea4de085704c969f5fc
admin16 <admin16@admin16-PC>
2017-11-01 16:41:52 +0900

    Signed-off-by: admin16 <admin16@admin16-PC>

--- ex1101/src/ex1101_IO/Main.java
+++ ex1101/src/ex1101_IO/Main.java
@@ -1,10 +1,33 @@
 package ex1101_IO;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class Main {
 
 	public static void main(String[] args) {
-		String path = "C:\\test\test.txt";
-
+		String path = "C:\\test\\test.txt";
+		FileInputStream  fils = null;
+		try {
+			fils = new FileInputStream(new File(path));
+			
+			int input = 0;
+			
+			while((input = fils.read()) != -1) {
+				System.out.print((char)input);
+			}
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				fils.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}		
 	}
-
 }
